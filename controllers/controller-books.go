ba082f0a84c96e11cb55a09505fe75fe7ad0d6bb
Sun-Mountain <nzonnenberg@fearless.tech>
2021-02-01 16:43:02 -0500

    Delete added

--- controllers/controller-books.go
+++ controllers/controller-books.go
@@ -77,3 +77,18 @@ func UpdateBook(c *gin.Context) {
 
   c.JSON(http.StatusOK, gin.H{"data": book})
 }
+
+// DELETE /books/:id
+// Delete a book
+func DeleteBook(c *gin.Context) {
+	// Get model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
+}
--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	r.POST("/books", controllers.CreateBook)
 	r.GET("/books/:id", controllers.FindBook)
 	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 
   r.Run()
 }
